b43beb804de3721cdd634e2ce220ec9dc975bcbb
sushanshakya77 <sushanshakya77@gmail.com>
2022-06-14 21:26:11 +0545

    initial commit

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return <></>;
+}
+
+export default App;
