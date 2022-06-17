e64671f7f8fddff54bb12cb4f85642188b84491d
sushanshakya77 <sushanshakya77@gmail.com>
2022-06-17 13:22:55 +0545

    feat: added global wrappers

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
-
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
 );
