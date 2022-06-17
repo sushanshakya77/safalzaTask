7887e99e3fa28c3dc6b699678447bf8e5de382fc
sushanshakya77 <sushanshakya77@gmail.com>
2022-06-17 13:23:05 +0545

    feat: added routes

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,17 @@
-import { Button } from '@chakra-ui/react';
-import styled from 'styled-components';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { Route, Routes } from 'react-router';
+import BaseLayout from './Pages/BaseLayout';
+import BranchMembers from './Pages/BranchMembers';
+import BranchOffice from './Pages/BranchOffice';
 
 export function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<BaseLayout />}>
+        <Route path="/users" element={<BranchMembers />} />
+        <Route index element={<BranchOffice />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
